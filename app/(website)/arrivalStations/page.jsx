9b077ede6a96c47d1cf24b4b9c6ae37176bff6bc
aditya <asonwal5@gmail.com>
2024-11-06 18:32:13 +0530

    static station add

--- app/(website)/arrivalStations/page.jsx
+++ app/(website)/arrivalStations/page.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { message, Collapse } from "antd";
 import VendorCard from "./VendorCard";
+import RecentOrders from "@/app/componants/RecentOrders";
 
 const { Panel } = Collapse;
 
@@ -11,7 +12,13 @@ const ArrivalStations = () => {
   const trainNo = searchParams.get("trainNo");
   const trainName = searchParams.get("trainName");
   const journeyDate = searchParams.get("date");
-  const [upcomingStations, setUpcomingStations] = useState([]);
+  const [upcomingStations, setUpcomingStations] = useState([
+    {
+      station_name: "jaipur",
+      station_code: "CEN",
+      eta: "10:30 AM",
+    },
+  ]);
   const [activeKey, setActiveKey] = useState(null);
 
   useEffect(() => {
@@ -28,13 +35,12 @@ const ArrivalStations = () => {
 
         if (response.ok && result.data.success) {
           setUpcomingStations(result.data.upcoming_stations);
+          console.log(result.data.upcoming_stations); 
         } else {
           throw new Error(result.message || "Failed to fetch station data.");
         }
       } catch (error) {
-        if (process.env.NODE_ENV !== "production") {
-          message.error(`Error: ${error.message}`);
-        }
+        message.error(`Error: ${error.message}`);
       }
     };
 
@@ -45,12 +51,12 @@ const ArrivalStations = () => {
 
   return (
     <div className="max-w-3xl mx-auto p-4">
-      <h1 className="text-2xl font-bold text-center mb-4" style={{ color: '#704d25' }}>
+      <h1 className="text-2xl font-bold text-center mb-4" style={{color:'#704d25'}}>
         Order Food in {trainName} ({trainNo}) Train
       </h1>
 
       <div className="bg-gray-100 p-2 mb-4">
-        <span className="font-semibold" style={{ color: '#704d25' }}>Journey Date:</span> {journeyDate}
+        <span className="font-semibold" style={{color:'#704d25'}}>Journey Date:</span> {journeyDate}
       </div>
 
       <Collapse accordion activeKey={activeKey} onChange={setActiveKey}>
@@ -77,7 +83,9 @@ const ArrivalStations = () => {
           </Panel>
         ))}
       </Collapse>
+      <RecentOrders/>
     </div>
+    
   );
 };
 
