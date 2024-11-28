292fa769e66e50bf635e219de35284b4c9dfc970
aditya <asonwal5@gmail.com>
2024-11-28 23:07:44 +0530

    use cloadanary for images

---
+++ app/utils/cloudinary.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Add in .env.local
+  api_key: process.env.CLOUDINARY_API_KEY,      // Add in .env.local
+  api_secret: process.env.CLOUDINARY_API_SECRET // Add in .env.local
+});
+
+export default cloudinary;
