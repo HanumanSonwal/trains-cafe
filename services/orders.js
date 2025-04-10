c79cb5584b5f21393f5854f32aa2344cc0434683
Hanuman Sonwal <hanuman.sonwal@nextdynamix.com>
2025-04-10 13:11:17 +0530

    checkout fuction correct

--- services/orders.js
+++ services/orders.js
@@ -40,7 +40,9 @@ export const placeOrder = async (
     train,
     payment,
     cart,
-    user_details
+    user_details,
+couponCode,   // 👈 add kiya
+discount 
 ) => {
     try {
         const response = await fetch("/api/orders/place-order", {
@@ -54,7 +56,9 @@ export const placeOrder = async (
                 train: train,
                 payment: payment,
                 cart: cart,
-                user_details: user_details
+                user_details: user_details,
+ couponCode: couponCode,  // 👈 add kiya
+ discount: discount       
             }),
         });
 
@@ -73,3 +77,48 @@ export const placeOrder = async (
         throw error;
     }
 };
+
+
+// export const placeOrder = async (
+//     vendor,
+//     station,
+//     train,
+//     payment,
+//     cart,
+//     user_details,
+//     couponCode,   // 👈 add kiya
+//     discount      // 👈 add kiya
+// ) => {
+//     try {
+//         const response = await fetch("/api/orders/place-order", {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify({
+//                 vendor: vendor,
+//                 station: station,
+//                 train: train,
+//                 payment: payment,
+//                 cart: cart,
+//                 user_details: user_details,
+//                 couponCode: couponCode,  // 👈 add kiya
+//                 discount: discount       // 👈 add kiya
+//             }),
+//         });
+
+//         const result = await response.json();
+
+//         console.log("API response:", response);
+//         console.log("Parsed result:", result);
+
+//         if (response.ok && result.success) {
+//             return result;
+//         } else {
+//             throw new Error(result.message || "Failed to place order.");
+//         }
+//     } catch (error) {
+//         console.error("Error in placeOrder:", error);
+//         throw error;
+//     }
+// };
