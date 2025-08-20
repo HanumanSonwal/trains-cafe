6fcbdb1f65c53516a23824f9d615946ef41d6cec
aditya <asonwal5@gmail.com>
2025-08-20 18:43:45 +0530

    set home page

--- app/(website)/page.js
+++ app/(website)/page.js
@@ -49,7 +49,7 @@ export default function Home() {
   return (
     <div>
       
-      {/* <HeroSection /> */}
+      <HeroSection />
       <OrderFood />
       <StepsSection />
       <PromoBanner />
