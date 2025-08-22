6c92e346fe0209851d31362b9e19706294dcbbf1
aditya <asonwal5@gmail.com>
2025-08-22 22:02:55 +0530

    changes sitemap url

--- app/api/sitemap/route.js
+++ app/api/sitemap/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  const baseUrl = process.env.NEXT_PUBLIC_URL || "http://localhost:3000";
 
   const staticPages = [
     { slug: "/" },
