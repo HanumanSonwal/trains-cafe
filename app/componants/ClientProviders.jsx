d25c050ba4c624d4bb0389e39c2c617683c21edb
Aditya Sonwal <asonwal5@gmail.com>
2025-12-27 14:34:42 +0530

    done seo tree

--- app/componants/ClientProviders.jsx
+++ app/componants/ClientProviders.jsx
@@ -3,13 +3,13 @@
 import { Provider } from "react-redux";
 import store from "../redux/store";
 import GlobalLoaderProvider from "./GlobalLoaderContext";
-import SeoNavigation from "./SeoNavigation";
+// import SeoNavigation from "./SeoNavigation";
 
 export default function ClientProviders({ children }) {
   return (
     <Provider store={store}>
       <GlobalLoaderProvider>
-        <SeoNavigation />
+        {/* <SeoNavigation /> */}
         {children}
       </GlobalLoaderProvider>
     </Provider>
