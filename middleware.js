4ed5ff8f1cdbbc98fb2afac51a16af6b75fbe730
Aditya Sonwal <asonwal5@gmail.com>
2026-03-06 21:21:30 +0530

    under maintinance add

--- middleware.js
+++ middleware.js
@@ -2,12 +2,26 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 const secret = process.env.NEXTAUTH_SECRET;
+const MAINTENANCE_MODE = true;
 
 export async function middleware(req) {
   const token = await getToken({ req, secret });
 
   const { pathname } = req.nextUrl;
 
+  // 🔹 Maintenance Mode
+  if (
+    MAINTENANCE_MODE &&
+    !pathname.startsWith("/admin") &&
+    !pathname.startsWith("/admin-auth") &&
+    !pathname.startsWith("/under-maintinance") &&
+    !pathname.startsWith("/api") &&
+    !pathname.startsWith("/_next")
+  ) {
+    return NextResponse.redirect(new URL("/under-maintinance", req.url));
+  }
+
+  // 🔹 Admin Auth Check
   if (pathname.startsWith("/admin")) {
     if (!token) {
       return NextResponse.redirect(new URL("/admin-auth", req.url));
@@ -18,5 +32,5 @@ export async function middleware(req) {
 }
 
 export const config = {
-  matcher: ["/admin/:path*"],
+  matcher: ["/:path*"],
 };
