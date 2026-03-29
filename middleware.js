259a0ec450a44e7e8ada24033c18736990cef141
Aditya Sonwal <asonwal5@gmail.com>
2026-03-29 17:49:28 +0530

    MAINTENANCE_MODE  off

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 const secret = process.env.NEXTAUTH_SECRET;
-const MAINTENANCE_MODE = true; // Set to true to enable maintenance mode, false to disable
+const MAINTENANCE_MODE = false; // Set to true to enable maintenance mode, false to disable
 
 export async function middleware(req) {
   const token = await getToken({ req, secret });
